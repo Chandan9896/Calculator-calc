4b7b4d425395455091ffe6e79bfa5ecfc9438654
Chandan H P <chandanchintu7774@gmail.com>
2022-10-14 12:26:37 +0530

    first commit

---
+++ BasicCalculatorServer/Controllers/CalculatorController.cs
@@ -0,0 +1,41 @@
+﻿using BasicCalculatorServer.Model;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace BasicCalculatorServer.Controllers
+{
+    [Route("api/calculator")]
+    public class CalculatorController : Controller
+    {
+        [HttpGet("fnum/{fnum}/snum/{snum}/operation/{operation}")]
+        public async Task<double> Get(double fnum, double snum, string operation)
+        {
+
+            var res = operation switch
+            {
+                "add" => fnum + snum,
+                "subtract" => fnum - snum,
+                "multiply" => fnum * snum,
+                "divide" => fnum / snum,
+            };
+
+            var dbModel = new CalculatorDbModel();
+            dbModel.Id = new Guid();
+            dbModel.CreatedOn = DateTime.Now;
+            dbModel.fnum = fnum;
+            dbModel.snum = snum;
+            dbModel.otr = operation;
+            dbModel.res = res;
+            var collections = GetCollections("CalculatorDbModel");
+            collections.InsertOne(dbModel);
+            return res;
+        }
+
+        public IMongoCollection<CalculatorDbModel> GetCollections(string collectionName)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("Calculator");
+            return database.GetCollection<CalculatorDbModel>(collectionName);
+        }
+    }
+}
---
+++ BasicCalculatorServer/Models/CalculatorDbModel.cs
@@ -0,0 +1,17 @@
+﻿namespace BasicCalculatorServer.Model
+{
+    public class CalculatorDbModel
+    {
+        public Guid Id { get; set; }
+
+        public double fnum { get; set; }
+
+        public double snum { get; set; }
+
+        public string otr { get; set; }
+
+        public double res { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
